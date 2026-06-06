e8fac1608042379222bdac237ca1e3e84cb03c0f
ivamly <ivamly.dev@gmail.com>
2026-06-06 09:58:15 +0300

    feat(chill): add delete chill

--- src/main/java/ru/ivamly/chill/controller/ChillController.java
+++ src/main/java/ru/ivamly/chill/controller/ChillController.java
@@ -3,6 +3,7 @@ package ru.ivamly.chill.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,5 +42,11 @@ public class ChillController { // TODO добавить сваггер
         return chillMapper.mapToGetChillRs(
                 chillService.get(id)
         );
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable UUID id) {
+        chillService.delete(id);
     }
 }
---
+++ src/test/java/ru/ivamly/chill/it/DeleteChillTest.java
@@ -0,0 +1,60 @@
+package ru.ivamly.chill.it;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.ResultActions;
+import ru.ivamly.chill.entity.Chill;
+import ru.ivamly.chill.entity.enums.ChillType;
+import ru.ivamly.chill.repository.ChillRepository;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("Удалить chill")
+public class DeleteChillTest extends BaseIntegrationTest {
+
+    @Autowired
+    private ChillRepository chillRepository;
+
+    @Test
+    @SneakyThrows
+    @DisplayName("Удалить существующий chill")
+    void shouldDeleteExistingChill() {
+        // given
+        Chill chill = new Chill();
+        chill.setUserId(UUID.randomUUID());
+        chill.setType(ChillType.OFF);
+        chill.setStartDate(LocalDate.of(2026, Month.MAY, 5));
+        chill.setEndDate(LocalDate.of(2026, Month.MAY, 5));
+        Chill savedChill = chillRepository.save(chill);
+
+        // when
+        ResultActions resultAction = mockMvc.perform(delete("/api/1/chill/{id}", savedChill.getId()));
+
+        // then
+        resultAction.andExpect(status().isNoContent());
+        assertThat(chillRepository.findById(savedChill.getId())).isEmpty();
+    }
+
+    @Test
+    @SneakyThrows
+    @DisplayName("Удалить несуществующйи chill")
+    void shouldDeleteNonExistingChill() {
+        // given
+        UUID nonExistingId = UUID.randomUUID();
+
+        // when
+        ResultActions resultAction = mockMvc.perform(delete("/api/1/chill/{id}", nonExistingId));
+
+        // then
+        resultAction.andExpect(status().isNoContent());
+        assertThat(chillRepository.findById(nonExistingId)).isEmpty();
+    }
+}
