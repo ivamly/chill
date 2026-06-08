270a08fad858867173caafb8bb746475c1ba0beb
ivamly <ivamly.dev@gmail.com>
2026-06-08 22:03:38 +0300

    fix(chill): forbid update and delete chill when startDate >= current date

--- src/main/java/ru/ivamly/chill/entity/Chill.java
+++ src/main/java/ru/ivamly/chill/entity/Chill.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Version;
 import lombok.Getter;
 import lombok.Setter;
 import ru.ivamly.chill.entity.enums.ChillType;
@@ -23,4 +24,6 @@ public class Chill {
     private String comment;
     private LocalDate startDate;
     private LocalDate endDate;
+    @Version
+    private Long version;
 }
